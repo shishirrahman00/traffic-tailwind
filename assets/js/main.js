39cf4649dd63aed10e1a1b27be1d4b76624a1de6
Shishir Rahman Shuvo <shishirrahman23@gmail.com>
2024-12-11 21:28:41 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+const testimonialSlider = document.querySelector(".testimonial-slider");
+const caseStudySlider = document.querySelector(".case-study-slider");
+const caseSliderElement = document.querySelector(".case-slider");
+
+if (testimonialSlider) {
+  tns({
+    container: testimonialSlider,
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controls: false,
+  });
+}
+
+if (caseStudySlider) {
+  tns({
+    container: caseStudySlider,
+    items: 3,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controls: false,
+    gutter: 20,
+    responsive: {
+      425: {
+        gutter: 20,
+        items: 1,
+      },
+      768: {
+        gutter: 30,
+        items: 2,
+      },
+      1024: {
+        gutter: 20,
+        items: 3,
+      },
+    },
+  });
+}
+
+if (caseSliderElement) {
+  tns({
+    container: caseSliderElement,
+    items: 5,
+    slideBy: "page",
+    autoplay: true,
+  });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./home-page.html",
+    "./home-page-2.html",
+    "./home-page-3.html",
+    "./home-page-4.html",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1170px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
